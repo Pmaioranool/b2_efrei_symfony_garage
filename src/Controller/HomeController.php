b53162fb1f99621f0eb416aa5be00417e7f9c854
Pmaioranool <lucasmaiorano77@gmail.com>
2024-09-30 18:17:40 +0200

    Controller

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -13,4 +13,21 @@ class HomeController extends AbstractController
     {
         return $this->render('home.html.twg');
     }
+
+    #[Route('/', '', methods: ['GET'])]
+    public function compte(): Response
+    {
+        return $this->render('compte.html.twig');
+    }
+    #[Route('/', '', methods: ['GET'])]
+    public function administation(): Response
+    {
+        return $this->render('administration.html.twig');
+    }
+    #[Route('/', '', methods: ['GET'])]
+    public function collection(): Response
+    {
+        return $this->render('catlogue.html.twig');
+    }
+
 }
