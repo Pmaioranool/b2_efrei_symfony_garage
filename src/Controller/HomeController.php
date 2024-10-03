635f366fd35a7a851bacd2d6b5b8407df1fd1e7d
Pmaioranool <lucasmaiorano77@gmail.com>
2024-10-03 18:26:59 +0200

    avant dernier ajout

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,39 +2,47 @@
 
 namespace App\Controller;
 
+use App\Repository\ArticleRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController
 {
-    #[Route('/', 'home.index', methods: ['GET'])]
+    #[Route('/', name: 'home.index', methods: ['GET'])]
     public function index(): Response
     {
         return $this->render('home.html.twig');
     }
 
-    #[Route('/compte', 'home.compte', methods: ['GET'])]
+    #[Route('/compte', name: 'home.compte', methods: ['GET'])]
     public function compte(): Response
     {
         return $this->render('compte.html.twig');
     }
-    #[Route('/administration', 'home.administration', methods: ['GET'])]
+
+    #[Route('/administration', name: 'home.administration', methods: ['GET'])]
     public function administration(): Response
     {
         return $this->render('administration.html.twig');
     }
-    #[Route('/catalogue', 'home.catalogue', methods: ['GET'])]
-    public function catalogue(): Response
+
+    #[Route('/catalogue', name: 'home.catalogue', methods: ['GET'])]
+    public function catalogue(ArticleRepository $articleRepository): Response
     {
-        return $this->render('catalogue.html.twig');
+        $articles = $articleRepository->findAll();
+        return $this->render('catalogue.html.twig', [
+            'articles' => $articles
+        ]);
     }
-    #[Route('/item', 'home.item', methods: ["GET"])]
+
+    #[Route('/item', name: 'home.item', methods: ['GET'])]
     public function item(): Response
     {
         return $this->render('item.html.twig');
     }
-    #[Route('/avis', 'home.avis', methods: ["GET"])]
+
+    #[Route('/avis', name: 'home.avis', methods: ['GET'])]
     public function avis(): Response
     {
         return $this->render('avis.html.twig');
